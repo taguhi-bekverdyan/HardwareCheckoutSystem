685cf4e70a93273f958d98a9351f0b2e7d8c1ba5
Karen Pirumyan <k.pirumyan@gmail.com>
2018-11-12 19:45:57 +0400

    added IEquatable to Brands and Categories

--- HardwareCheckoutSystemAdmin/HardwareCheckoutSystemAdmin.Models/Brand.cs
+++ HardwareCheckoutSystemAdmin/HardwareCheckoutSystemAdmin.Models/Brand.cs
@@ -5,10 +5,15 @@ using System.ComponentModel.DataAnnotations;
 
 namespace HardwareCheckoutSystemAdmin.Models
 {
-  public class Brand
+  public class Brand : IEquatable<Brand>
   {
     [Key, ValidGuid]
     public Guid Id { get; set; }
     public string Name { get; set; }
+
+    public bool Equals(Brand other)
+    {
+      return Id == other.Id;
+    }
   }
 }
--- HardwareCheckoutSystemAdmin/HardwareCheckoutSystemAdmin.Models/Category.cs
+++ HardwareCheckoutSystemAdmin/HardwareCheckoutSystemAdmin.Models/Category.cs
@@ -5,10 +5,15 @@ using System.ComponentModel.DataAnnotations;
 
 namespace HardwareCheckoutSystemAdmin.Models
 {
-  public class Category
+  public class Category : IEquatable<Category>
   {
     [Key, ValidGuid]
     public Guid Id { get; set; }
     public string Name { get; set; }
+
+    public bool Equals(Category other)
+    {
+      return Id == other.Id;
+    }
   }
 }
