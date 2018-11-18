54b52c4c7ec4717e89751c34a88bfd1c0f34bb55
Karen Pirumyan <k.pirumyan@gmail.com>
2018-11-18 14:14:17 +0400

    added migrations

--- HCSWebAPI/HCSWebAPI/Controllers/DevicesController.cs
+++ HCSWebAPI/HCSWebAPI/Controllers/DevicesController.cs
@@ -18,6 +18,42 @@ namespace HCSWebAPI.Controllers
     public DevicesController(DataContext context)
     {
       _service = new DeviceService(context);
+
+      Seed(context);
+    }
+
+    private void Seed(DataContext context)
+    {
+      if (context.Brands.Count() == 0)
+      {
+        List<Brand> brands = new List<Brand> {
+          new Brand { Id = Guid.NewGuid(), Name = "Dell"},
+          new Brand { Id = Guid.NewGuid(), Name = "Lenovo" },
+          new Brand { Id = Guid.NewGuid(), Name = "Acer" },
+          new Brand { Id = Guid.NewGuid(), Name = "Apple" },
+          new Brand { Id = Guid.NewGuid(), Name = "Toshiba" },
+          new Brand { Id = Guid.NewGuid(), Name = "Canon" },
+          new Brand { Id = Guid.NewGuid(), Name = "Logitec" },
+          new Brand { Id = Guid.NewGuid(), Name = "HP" },
+          new Brand { Id = Guid.NewGuid(), Name = "TP-Link" },
+          new Brand { Id = Guid.NewGuid(), Name = "D-Link" }
+        };
+        context.Brands.AddRange(brands);
+      }
+
+      if (context.Categories.Count() == 0)
+      {
+        List<Category> categories = new List<Category> {
+          new Category { Id = Guid.NewGuid(), Name = "Desktop Computers"},
+          new Category { Id = Guid.NewGuid(), Name = "Servers" },
+          new Category { Id = Guid.NewGuid(), Name = "Monitors" },
+          new Category { Id = Guid.NewGuid(), Name = "Accessories" },
+          new Category { Id = Guid.NewGuid(), Name = "Network Equipments" },
+          new Category { Id = Guid.NewGuid(), Name = "Printers and Scanners" },
+          new Category { Id = Guid.NewGuid(), Name = "Laptops" }
+        };
+        context.Categories.AddRange(categories);
+      }
     }
 
     // GET: api/Devices
